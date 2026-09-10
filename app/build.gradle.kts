d8a2a3f90e05a44decb0d0368dc6731ee63feb61
astrlstp-crypto <astrlstp@gmail.com>
2026-09-10 20:07:26 +0300

    Add app module

---
+++ app/build.gradle.kts
@@ -0,0 +1,13 @@
+plugins { id("com.android.application") }
+
+android {
+    namespace = "com.kupchino.app"
+    compileSdk = 35
+    defaultConfig {
+        applicationId = "com.kupchino.app"
+        minSdk = 24
+        targetSdk = 35
+        versionCode = 1
+        versionName = "1.0"
+    }
+}
